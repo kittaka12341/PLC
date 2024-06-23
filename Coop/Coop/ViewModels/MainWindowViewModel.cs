f5e7f5fa4a6f2fb6b670eda2537b35f0888e82bf
s.kittaka <ninato.ksagmkhy@gmail.com>
2024-06-23 10:23:09 +0900

    Prism DryIoc DIのHelloWorldができた

---
+++ Coop/Coop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using UIContents.Views;
+
+namespace Coop.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private readonly IRegionManager regionManager;
+
+        public MainWindowViewModel(IRegionManager _regionManager)
+        {
+            this.regionManager = _regionManager;
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ServerSim));
+        }
+    }
+}
